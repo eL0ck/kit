d0663b935b4717a3d1c613ec6e83aab11af647c7
eL0ck <tpj800@gmail.com>
2018-04-28 20:30:25 +1000

    Trying to get Pypi working

--- ci/build_deploy.sh
+++ ci/build_deploy.sh
@@ -1,10 +1,5 @@
-#! /bin/bash
-
-if [[ -z "$PYPI_USERNAME" || -z "$PYPI_PASSWORD" || -z "$PYPI_URL" ]]; then
-    echo "You must set PYPI_USERNAME, PYPI_PASSWORD, and PYPI_URL to run this script"
-    exit 1
-fi
-echo "Required environment variables are set ..."
+#!/bin/bash
+set -ev
 
 # Write the short version into file and make a long version with the shorthash
 SHORT_HASH=$(git rev-parse --short HEAD)  # also: TRAVIS_COMMIT
@@ -20,18 +15,5 @@ echo ${BUILD_VERSION} > version.txt
 BUILD_VERSION=${BUILD_VERSION}-${SHORT_HASH}
 echo "Could use the following for conda: $BUILD_VERSION"
 
-echo "Setting up pypi connection info ..."
-cat << EOF > /root/.pypirc
-[distutils]
-index-servers=pypi
-
-[pypi]
-repository=${PYPI_URL}
-username=${PYPI_USERNAME}
-password=${PYPI_PASSWORD}
-EOF
-
-echo "Finally: Building package ..."
-python setup.py sdist bdist_wheel
-echo "Uploading to pypi ($PYPI_URL) ..."
-twine upload dist/*
+echo "New version of 'version.txt': "
+cat version.txt
